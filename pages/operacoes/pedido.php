88ba624b5a7145bf21af744ceed66428a6164569
Johnnathan Pacífico <106749308+JPFS2@users.noreply.github.com>
2023-06-18 15:27:55 -0300

    1

--- pages/operacoes/pedido.php
+++ pages/operacoes/pedido.php
@@ -146,12 +146,12 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Pedido</h1>
+                            <h1>Carrinho</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="../../index.html">Inicio</a></li>
-                                <li class="breadcrumb-item active">Pedido</li>
+                                <li class="breadcrumb-item active">Carrinho</li>
                             </ol>
                         </div>
                     </div>
